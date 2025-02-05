4f41e95ebf27d0f28e6fa9baada0441af6052f64
Amansingh2202 <118821685+Amansingh2202@users.noreply.github.com>
2025-02-05 19:42:42 +0530

    Time: 3 ms (34.22%) | Memory: 15.3 MB (25.34%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void powerset(int n,vector<int>nums,vector<int> &dummy , vector<vector<int >>&ans){
+        if(n<0)
+        {
+            ans.push_back(dummy);
+            return ;
+        }
+        
+    
+    dummy.push_back(nums[n]);
+    powerset(n-1,nums,dummy,ans);
+    dummy.pop_back();
+    powerset(n-1,nums,dummy,ans);
+
+
+    }
+    
+
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+       vector<vector<int>>ans;
+        vector<int>dummy;
+        powerset(n-1,nums,dummy,ans);
+        return ans;
+    }
+};
