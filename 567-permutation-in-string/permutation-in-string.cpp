23d4362fe7a567a8b1f8c9545fcc752ae56d3186
Amansingh2202 <118821685+Amansingh2202@users.noreply.github.com>
2025-04-28 23:03:18 +0530

    Time: 2 ms (71.09%) | Memory: 10.1 MB (35.39%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int>mp, mp2;
-        for(auto ch:s1) mp2[ch]++;
+        vector<int> mp1(26, 0), mp2(26, 0);
+    int k = s1.size(), n = s2.size();
 
-        int i = 0, j = 0, k = s1.size(), n = s2.size();
-        while(j < n)
-        {
-            mp[s2[j++]]++;
-            if(j - i == k)
-            {
-                if(mp == mp2) return true;
-                mp[s2[i]]--;
-                if(mp[s2[i]] == 0) mp.erase(s2[i]);
-                i++;
-            }
-        }
-        return false;
+    if(k > n) return false;
+
+    for(char ch : s1) mp1[ch - 'a']++;
+
+    int matchCount = 0;
+    for(int i = 0; i < k; i++) {
+        mp2[s2[i] - 'a']++;
+    }
+    for(int i = 0; i < 26; i++) {
+        if(mp1[i] == mp2[i]) matchCount++;
+    }
+
+    for(int i = k; i < n; i++) {
+        if(matchCount == 26) return true;
+
+        int in = s2[i] - 'a';       // new character entering the window
+        int out = s2[i - k] - 'a';  // character leaving the window
+
+        mp2[in]++;
+        if(mp2[in] == mp1[in]) matchCount++;
+        else if(mp2[in] - 1 == mp1[in]) matchCount--;
+
+        mp2[out]--;
+        if(mp2[out] == mp1[out]) matchCount++;
+        else if(mp2[out] + 1 == mp1[out]) matchCount--;
+    }
+
+    return matchCount == 26;
     }
 };
